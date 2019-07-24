54814b7d47f8461b0ba45698edcc6262516b5e7d
shde747 <sh.de747@gmail.com>
2019-07-24 10:40:14 +0300

    Updated skeleton

--- hmm/hmm_driver.py
+++ hmm/hmm_driver.py
@@ -1,5 +1,13 @@
 from nltk.tag.hmm import *
 
 print('Preparing data')
+train_set = [[],[]]
+test_set = [[],[]]
+symbols = []
+
 print('Training')
+hmm_trainer = HiddenMarkovModelTrainer(states = ['a','e','u','i','o','*'], symbols=symbols)
+model = hmm_trainer.train(labeled_sequences=train_set)
+
 print('Prediction')
+pred_set = model.tag(test_set[0])

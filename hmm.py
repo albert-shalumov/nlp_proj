26edbd32792e294d238439b737ef80898d934f0a
shde747 <sh.de747@gmail.com>
2019-07-25 10:21:11 +0300

    Added ngrams

--- hmm.py
+++ hmm.py
@@ -4,51 +4,70 @@ import numpy as np
 from sklearn.metrics import confusion_matrix
 import metrics
 
-print('Preparing data')
-data = list()
-symbols = set()
 states = ['a','e','u','i','o','*']
 state_idx = {x:i for i,x in enumerate(states)}
 
-with codecs.open('data/HaaretzOrnan_annotated.txt', encoding='utf-8') as f:
-    lines = f.readlines()
-    for line in lines:
-        line = line.rstrip()
-        if line.startswith(u'#') or len(line)==0:
-            continue
-        w = line.split(u' ')[3]
-        w = w.replace(u'-', u'')
-        cons = list(w[::2])
-        vowel = list(w[1::2])
-        data.append(list(zip(cons, vowel)))
-        symbols.update(cons)
+def ExtractNgrams(word, n=1):
+    start_symb=u'-'
+    start_w = [start_symb]*(n-1)
+    w = start_w+list(word)
+    l = []
+    for i in range(len(w)-n+1):
+        l.append(''.join(w[i:i+n]))
+    return l
 
 
-iterations = 50
-conf_mat = np.zeros((len(states),len(states)))
-for i in range(iterations):
-    np.random.shuffle(data)
-    valid_size = max(1, len(data)//7)
-    train = data[:-valid_size]
-    valid = data[-valid_size:]
+def TestNgram(n=1,iters=5):
+    data = list()
+    symbols = set()
 
-    print('Training {}/{}'.format(i,iterations))
-    hmm_trainer = HiddenMarkovModelTrainer(states = ['a','e','u','i','o','*'], symbols=list(symbols))
-    model = hmm_trainer.train(labeled_sequences=train)
-    #print('Prediction')
-    valid_word_cons = [[x[0] for x in w] for w in valid]
-    valid_word_vowel = [[x[1] for x in w] for w in valid]
+    #print('Preparing data')
+    with codecs.open('data/HaaretzOrnan_annotated.txt', encoding='utf-8') as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.rstrip()
+            if line.startswith(u'#') or len(line)==0:
+                continue
+            w = line.split(u' ')[3]
+            w = w.replace(u'-', u'')
+            ngrams = ExtractNgrams(w[::2], n)
+            vowel = list(w[1::2])
+            data.append(list(zip(ngrams, vowel)))
+            symbols.update(ngrams)
 
-    #print('Results:')
-    for i,w_cons in enumerate(valid_word_cons):
-    #    print('     ',w_cons)
-    #    print('Gold:',valid_word_vowel[i])
-        pred_set = model.best_path(w_cons)
-        for j in range(len(pred_set)):
-            conf_mat[state_idx[pred_set[j]],state_idx[valid_word_vowel[i][j]]] += 1
-    #    print('Pred:',pred_set)
-    #    print('-------------------------------')
-print('MicroAvg:',metrics.MicroAvg(conf_mat))
-print('MacroAvg:',metrics.MacroAvg(conf_mat))
-print('AvgAcc:',metrics.AvgAcc(conf_mat))
-print('ConfMat:\n', np.array_str(conf_mat, max_line_width=300, precision=4))
+
+    conf_mat = np.zeros((len(states),len(states)))
+
+    for i in range(iters):
+        np.random.shuffle(data)
+        valid_size = max(1, len(data)//10)
+        train = data[:-valid_size]
+        valid = data[-valid_size:]
+
+        #print('Training {}/{}'.format(i,iters))
+        hmm_trainer = HiddenMarkovModelTrainer(states = states, symbols = list(symbols))
+        model = hmm_trainer.train(labeled_sequences=train)
+        #print('Prediction')
+        valid_word_cons = [[x[0] for x in w] for w in valid]
+        valid_word_vowel = [[x[1] for x in w] for w in valid]
+
+        #print('Results:')
+        for i,w_cons in enumerate(valid_word_cons):
+        #    print('     ',w_cons)
+        #    print('Gold:',valid_word_vowel[i])
+            pred_set = model.best_path(w_cons)
+            for j in range(len(pred_set)):
+                conf_mat[state_idx[pred_set[j]],state_idx[valid_word_vowel[i][j]]] += 1
+        #    print('Pred:',pred_set)
+        #    print('-------------------------------')
+    return conf_mat
+
+
+for i in range(1,6,1):
+    print("|Ngram| = {}: ".format(i))
+    conf_mat = TestNgram(i, 100)
+    print('MicroAvg:',metrics.MicroAvg(conf_mat))
+    print('MacroAvg:',metrics.MacroAvg(conf_mat))
+    print('AvgAcc:',metrics.AvgAcc(conf_mat))
+    print('ConfMat:\n', np.array_str(conf_mat, max_line_width=300, precision=4))
+    print('----------------------------------------------')

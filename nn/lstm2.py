00d447906338a2c0931e29029ae6c33666e25151
shde747 <sh.de747@gmail.com>
2019-08-13 11:26:00 +0300

    Finalized class for encoder-decoder architecture

--- nn/lstm2.py
+++ nn/lstm2.py
@@ -2,100 +2,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-#from embedding import Embedding,FastTextEmb
 import codecs
-import re
-import numpy as np
 import time
+import numpy as np
 import random
 
-PRED_STAGE = 1
-
-def BuildCharSets(sents):
-    sets = [set() for i in range(len(sents[0][0]))]
-    for sent in sents:
-        for word in sent:
-            word_list = list(word)
-            for set_id in range(len(word_list)):
-                sets[set_id].update(list(word_list[set_id]))
-    return sets
-
-def CalcMaxLen(sents):
-    max_len = 0
-    for sent in sents:
-        for word in sent:
-            word_list = list(word)
-            for set_id in range(len(word_list)):
-                max_len = max(max_len, len(word_list[set_id]))
-    return max_len
-
-def TensorsFromWordPair(word):
-    w = [dicts[0][1][ch] for ch in word[0][0]] + [dicts[0][1]['<e>']]
-    input_tensor = torch.tensor(w, dtype=torch.long, device=device).view(-1, 1)
-
-    w = [dicts[1][1][ch] for ch in word[0][1]] + [dicts[1][1]['<e>']]
-    target_tensor = torch.tensor(w, dtype=torch.long, device=device).view(-1, 1)
-    return (input_tensor, target_tensor)
-
-sentences = [[]]
-'''
-# For now ignore sentence context
-with codecs.open('../data/HaaretzOrnan_annotated.txt', encoding='utf-8') as f:
-    lines = f.readlines()
-    for line in lines:
-        line = line.rstrip()
-        if line.startswith(u'#'):
-            continue
-        if len(line) == 0:
-            if len(sentences[-1])>0:
-                sentences.append([])
-            continue
-        split_line = line.split(u' ')
-        sentences[-1].append((split_line[2],split_line[3],split_line[4]))
-if len(sentences[-1])==0:
-    sentences.pop(-1)
-
-# Add single word sentences
-for i in range(len(sentences)):
-    sentences += [[x] for x in sentences[i]]
-'''
-with codecs.open('../data/HaaretzOrnan_annotated.txt', encoding='utf-8') as f:
-    lines = f.readlines()
-    for line in lines:
-        line = line.rstrip()
-        if line.startswith(u'#'):
-            continue
-        if len(line) == 0:
-            continue
-        split_line = line.split(u' ')
-        sentences[-1] += [(split_line[2],split_line[3] if PRED_STAGE==1 else split_line[4])]
-        sentences.append([])
-if len(sentences[-1])==0:
-    sentences.pop(-1)
-max_len = CalcMaxLen(sentences)+5
-START_SYMB=0
-
-
-sets = BuildCharSets(sentences)
-for set in sets: # custom chars
-    set.update(['<e>'])
-
-dicts = []
-for set in sets:
-    int2char = dict(enumerate(list(set)))
-    char2int = {char: ind for ind, char in int2char.items()}
-    zero_ch = int2char[0] # zero reserved - swap out
-    int2char[len(set)]=zero_ch
-    char2int[zero_ch] = len(set)
-    dicts.append((int2char, char2int))
-
-
-
-
-
-'''
-from: https://pytorch.org/tutorials/intermediate/seq2seq_translation_tutorial.html
-'''
 class EncoderRNN(nn.Module):
     def __init__(self, input_size, hidden_size):
         super(EncoderRNN, self).__init__()
@@ -113,28 +24,9 @@ class EncoderRNN(nn.Module):
     def initHidden(self):
         return torch.zeros(1, 1, self.hidden_size)
 
-class DecoderRNN(nn.Module):
-    def __init__(self, hidden_size, output_size):
-        super(DecoderRNN, self).__init__()
-        self.hidden_size = hidden_size
-
-        self.embedding = nn.Embedding(output_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
-        self.out = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
-
-    def forward(self, input, hidden):
-        output = self.embedding(input).view(1, 1, -1)
-        output = F.relu(output)
-        output, hidden = self.gru(output, hidden)
-        output = self.softmax(self.out(output[0]))
-        return output, hidden
-
-    def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size)
 
 class AttnDecoderRNN(nn.Module):
-    def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=max_len):
+    def __init__(self, hidden_size, output_size, dropout_p, max_length):
         super(AttnDecoderRNN, self).__init__()
         self.hidden_size = hidden_size
         self.output_size = output_size
@@ -167,95 +59,230 @@ class AttnDecoderRNN(nn.Module):
         return output, hidden, attn_weights
 
     def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device=device)
+        return torch.zeros(1, 1, self.hidden_size)
 
-teacher_forcing_ratio = 0.5
-device = 'cpu'
 
-def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, max_length=max_len):
-    encoder_hidden = encoder.initHidden()
+'''
+Class for Encoder-Decoder RNN method.
+Most functions return self, therefore calls can be chained: nn.shuffle().split().train() etc.
+References:
+1) https://pytorch.org/tutorials/intermediate/seq2seq_translation_tutorial.html
+2) https://towardsdatascience.com/how-to-code-the-transformer-in-pytorch-24db27c8f9ec
+3) https://github.com/jadore801120/attention-is-all-you-need-pytorch
 
-    encoder_optimizer.zero_grad()
-    decoder_optimizer.zero_grad()
+Note:
+-----
+Current implementation is quite basic, without optimization.
 
-    input_length = input_tensor.size(0)
-    target_length = target_tensor.size(0)
+Params:
+-------
+hidden_dim - size of hidden dimension
+n_layers - tuple (encoder layers, decoder layers)
+stage - 1 for vowel&syllab.,
+        2 for romanization
+device - force device ("cpu" to use cpu)
+'''
+class EncoderDecoder:
+    def __init__(self, hidden_dim=250, n_layers=(1,1), stage=1, device=None):
+        self.hid_dim = hidden_dim
+        self.n_layers = n_layers
+        self.stage = stage
+        self.proc_units = None
 
-    encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+        if device is None:
+            self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        else:
+            self.device = device
 
-    loss = 0
 
-    for ei in range(input_length):
-        encoder_output, encoder_hidden = encoder(
-            input_tensor[ei], encoder_hidden)
-        encoder_outputs[ei] = encoder_output[0, 0]
+    def prep_model(self, file='../data/HaaretzOrnan_annotated.txt'):
+        self.proc_units = [[]]
+        with codecs.open(file, encoding='utf-8') as f:
+            lines = f.readlines()
+            for line in lines:
+                line = line.rstrip()
+                if line.startswith(u'#'):
+                    continue
+                if len(line) == 0:
+                    continue
+                split_line = line.split(u' ')
+                self.proc_units[-1] += [(split_line[2], split_line[3] if self.stage == 1 else split_line[4])]
+                self.proc_units.append([])
+        if len(self.proc_units[-1]) == 0:
+            self.proc_units.pop(-1)
 
-    decoder_input = torch.tensor([[START_SYMB]], device=device)
+        # Calculate max unit length, dictionaries
+        self.max_len = EncoderDecoder.CalcMaxLen(self.proc_units)+5
+        unit_dict_sets = EncoderDecoder.BuildCharSets(self.proc_units)
 
-    decoder_hidden = encoder_hidden
+        # Add EOW symbol
+        unit_dict_sets[0].add('<e>')
+        unit_dict_sets[1].add('<e>')
 
-    use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+        # Init lookup tables
+        self.input_int2char, self.input_char2int = EncoderDecoder.CreateDictionaries(unit_dict_sets[0])
+        self.output_int2char, self.output_char2int = EncoderDecoder.CreateDictionaries(unit_dict_sets[1])
 
-    if use_teacher_forcing:
-        # Teacher forcing: Feed the target as the next input
-        for di in range(target_length):
-            decoder_output, decoder_hidden, decoder_attention = decoder(
-                decoder_input, decoder_hidden, encoder_outputs)
-            loss += criterion(decoder_output, target_tensor[di])
-            decoder_input = target_tensor[di]  # Teacher forcing
+        # Create encoder, decoder
+        self.encoder = EncoderRNN(len(unit_dict_sets[0])+1, self.hid_dim).to(self.device)
+        self.decoder = AttnDecoderRNN(self.hid_dim, len(unit_dict_sets[1])+1, 0.1, self.max_len).to(self.device)
 
-    else:
-        # Without teacher forcing: use its own predictions as the next input
-        for di in range(target_length):
-            decoder_output, decoder_hidden, decoder_attention = decoder(
-                decoder_input, decoder_hidden, encoder_outputs)
-            topv, topi = decoder_output.topk(1)
-            decoder_input = topi.squeeze().detach()  # detach from history as input
+        return self
 
-            loss += criterion(decoder_output, target_tensor[di])
-            if decoder_input.item() == dicts[1][1]['<e>']:
-                break
 
-    loss.backward()
+    def shuffle(self, seed=None):
+        self.inds = np.arange(len(self.proc_units))
+        np.random.seed(seed)
+        np.random.shuffle(self.inds)
+        return self
 
-    encoder_optimizer.step()
-    decoder_optimizer.step()
+    def split(self, valid_ratio=0.1):
+        num_train = int(len(self.inds)*(1-valid_ratio))
+        self.train_inds = self.inds[:num_train]
+        self.valid_inds = None if valid_ratio == 0 else self.inds[num_train:]
+        return self
 
-    return loss.item() / target_length
+    def train(self):
+        # Local constants
+        teacher_forcing_ratio = 0.5
 
-def trainIters(encoder, decoder, n_iters, print_every=1000, plot_every=100, learning_rate=0.01):
-    start = time.time()
-    plot_losses = []
-    print_loss_total = 0  # Reset every print_every
-    plot_loss_total = 0  # Reset every plot_every
+        enc_optim = optim.SGD(self.encoder.parameters(), lr=1e-2)
+        dec_optim = optim.SGD(self.decoder.parameters(), lr=1e-2)
+        criterion = nn.NLLLoss()
 
-    encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
-    decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
-    training_pairs = [TensorsFromWordPair(random.choice(sentences)) for i in range(n_iters)]
-    criterion = nn.NLLLoss()
+        training_pairs = [self._tensor(self.proc_units[i][0]) for i in self.train_inds]
+        #validation_pairs = [self._tensor(self.proc_units[i][0]) for i in self.valid_inds]
 
-    for iter in range(1, n_iters + 1):
-        training_pair = training_pairs[iter - 1]
-        input_tensor = training_pair[0]
-        target_tensor = training_pair[1]
+        for epoch in range(10):
+            loss_sum = 0
+            for iter in range(len(training_pairs)):  # single sample
+                training_pair = training_pairs[iter]
+                input_tensor = training_pair[0]
+                target_tensor = training_pair[1]
 
-        loss = train(input_tensor, target_tensor, encoder,
-                     decoder, encoder_optimizer, decoder_optimizer, criterion)
-        print_loss_total += loss
-        plot_loss_total += loss
+                encoder_hidden = self.encoder.initHidden()
+                enc_optim.zero_grad()
+                dec_optim.zero_grad()
+                input_length = input_tensor.size(0)
+                target_length = target_tensor.size(0)
+                encoder_outputs = torch.zeros(self.max_len, self.encoder.hidden_size, device=self.device)
 
-        if iter % print_every == 0:
-            print_loss_avg = print_loss_total / print_every
-            print_loss_total = 0
-            print('(%d %d%%) %.4f' % (iter, iter / n_iters * 100, print_loss_avg))
+                loss = 0
 
-        #if iter % plot_every == 0:
-        #    plot_loss_avg = plot_loss_total / plot_every
-        #    plot_losses.append(plot_loss_avg)
-        #    plot_loss_total = 0#
+                for ei in range(input_length):
+                    encoder_output, encoder_hidden = self.encoder(input_tensor[ei], encoder_hidden)
+                    encoder_outputs[ei] = encoder_output[0, 0]
 
-hidden_size = 256
-encoder1 = EncoderRNN(len(sets[0])+1, hidden_size).to(device)
-attn_decoder1 = AttnDecoderRNN(hidden_size, len(sets[1])+1).to(device)
+                decoder_input = torch.tensor([[EncoderDecoder.START_SYMB]], device=self.device)
+                decoder_hidden = encoder_hidden
 
-trainIters(encoder1, attn_decoder1, 7500, print_every=5)
+                if np.random.random() < teacher_forcing_ratio: # use teacher forcing - use target instead of predicted. faster training
+                    # Teacher forcing: Feed the target as the next input
+                    for di in range(target_length):
+                        decoder_output, decoder_hidden, decoder_attention = self.decoder(
+                            decoder_input, decoder_hidden, encoder_outputs)
+                        loss += criterion(decoder_output, target_tensor[di])
+                        decoder_input = target_tensor[di]  # Teacher forcing
+                else:
+                    # Without teacher forcing: use its own predictions as the next input
+                    for di in range(target_length):
+                        decoder_output, decoder_hidden, decoder_attention = self.decoder(
+                            decoder_input, decoder_hidden, encoder_outputs)
+                        topv, topi = decoder_output.topk(1)
+                        decoder_input = topi.squeeze().detach()  # detach from history as input
+
+                        loss += criterion(decoder_output, target_tensor[di])
+                        if decoder_input.item() == self.output_char2int['<e>']:
+                            break
+
+                loss.backward()
+                enc_optim.step()
+                dec_optim.step()
+
+                loss_value = loss.item()/target_length
+                loss_sum += loss_value
+            print(loss_sum/len(training_pairs))
+
+        return self
+
+    def eval(self):
+        conf_mat = 1
+        return conf_mat
+
+    def predict(self, pred_set):
+        res_set = []
+        with torch.no_grad():
+            for sentence in pred_set:
+                res_set.append([])
+                for word in sentence:
+                    res_set[-1].append([])
+                    input_tensor = self._tensor((word,))
+                    input_length = input_tensor.size()[0]
+                    encoder_hidden = self.encoder.initHidden()
+                    encoder_outputs = torch.zeros(self.max_len, self.encoder.hidden_size, device=self.device)
+
+                    for ei in range(input_length):
+                        encoder_output, encoder_hidden = self.encoder(input_tensor[ei], encoder_hidden)
+                        encoder_outputs[ei] += encoder_output[0, 0]
+
+                    decoder_input = torch.tensor([[EncoderDecoder.START_SYMB]], device=self.device)
+                    decoder_hidden = encoder_hidden
+
+                    for di in range(self.max_len):
+                        decoder_output, decoder_hidden, decoder_attention = self.decoder(
+                            decoder_input, decoder_hidden, encoder_outputs)
+                        topv, topi = decoder_output.data.topk(1)
+                        if topi.item() == self.output_char2int['<e>']:
+                            break
+                        else:
+                            res_set[-1][-1].append(self.output_int2char[topi.item()])
+                        decoder_input = topi.squeeze().detach()
+
+        return res_set
+
+    def _tensor(self, unit_pair):
+        w = [self.input_char2int[ch] for ch in unit_pair[0]]+[self.input_char2int['<e>']]
+        input_tensor = torch.tensor(w, dtype=torch.long, device=self.device).view(-1, 1)
+        if len(unit_pair)==2:
+            w = [self.output_char2int[ch] for ch in unit_pair[1]]+[self.output_char2int['<e>']]
+            target_tensor = torch.tensor(w, dtype=torch.long, device=self.device).view(-1, 1)
+            return (input_tensor, target_tensor)
+        else:
+            return input_tensor
+    START_SYMB = 0
+
+    @staticmethod
+    def BuildCharSets(proc_units):
+        sets = [set(), set()]
+        for unit_list in proc_units:
+            for unit in unit_list:
+                units = list(unit)
+                sets[0].update(list(units[0]))
+                sets[1].update(list(units[1]))
+        return sets
+
+    @staticmethod
+    def CalcMaxLen(proc_units):
+        max_len = 0
+        for unit_list in proc_units:
+            for unit in unit_list:
+                units = list(unit)
+                max_len = max(max_len, len(units[0]))
+                max_len = max(max_len, len(units[1]))
+        return max_len
+
+    @staticmethod
+    def CreateDictionaries(unit_set, reserve_zero=True):
+        int2char = dict(enumerate(list(unit_set)))
+        char2int = {char: ind for ind, char in int2char.items()}
+        if reserve_zero:  # zero reserved - swap out
+            zero_ch = int2char[0]
+            int2char[len(unit_set)] = zero_ch
+            int2char.pop(0)
+            char2int[zero_ch] = len(unit_set)
+        return int2char, char2int
+
+if __name__=='__main__':
+    enc_dec = EncoderDecoder(256, (1,1), 2, 'cpu')
+    enc_dec.prep_model().shuffle().split(0).train()
+    #print(enc_dec.predict([[u'ˀnšym',u'nršmym']]))

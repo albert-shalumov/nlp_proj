4fd608d40f9fe74b7fa0cfbd3ce1a1547aa6f2be
albert <albert@geosim.co.il>
2019-09-15 19:35:47 +0300

    Embedding MDS

---
+++ embedding_mds.py
@@ -0,0 +1,113 @@
+import numpy as np
+import codecs
+from scipy.stats import wasserstein_distance
+
+'''
+Use multi-dimensional scaling to map consonant to fewer dimension.
+Distance between consonants calculated as Wasserstein distance(=Earth Mover Distance) between vowel co-appearance distribution 
+'''
+class Embedding:
+    def __init__(self):
+        self.mapping = np.identity(len(Embedding.ARNON_CHARS))
+
+    def learn(self, file='../data/HaaretzOrnan_annotated.txt'):
+        prob_distr = Embedding._calc_corpus_dist(file)
+        dist = Embedding._calc_dist(prob_distr)
+        n = prob_distr.shape[0]
+        A = -0.5 * dist * dist
+        H = np.identity(n) - np.ones((n, n)) / n
+        B = np.linalg.multi_dot([H, A, H])
+        V, D = np.linalg.eig(B)
+        ids = np.argsort(V)[::-1]
+        eig_mat = np.diag(V[ids])
+        D = D[:, ids]
+        err_mat = np.zeros(n)+np.inf
+        for i in range(1, n):
+            err = Embedding._calc_err(dist, D, eig_mat, i)
+            if np.isfinite(err):
+                err_mat[i] = err
+        min_ind = np.argmin(err_mat)
+
+        self.mapping = np.empty((min_ind+1,dist.shape[0]))
+        for i in range(dist.shape[0]):
+            for j in range(min_ind+1):
+                self.mapping[j,i] = D[i,j]*np.sqrt(eig_mat[j,j])
+
+
+    def save(self, file):
+        np.save(file, self.mapping)
+
+    def load(self, file):
+        self.mapping = np.load(file)
+
+    def len(self):
+        return self.mapping.shape[0]
+
+    def __getitem__(self, ch):
+        return self.mapping[:, Embedding.ARNON_CHARS_IDX[ch]]
+
+    VOWELS = [u'a', u'e', u'u', u'i', u'o', u'*']
+    VOWELS_IDX = {x: i for i, x in enumerate(VOWELS)}
+    ARNON_CHARS = [u'\u02c0', u'b', u'g', u'd', u'h', u'w', u'z', u'\u1e25', u'\u1e6d', u'y',
+                   u'k', u'k', u'l', u'm', u'm', u'n', u'n', u's', u'\u02c1', u'p', u'p', u'\u00e7',
+                   u'\u00e7', u'q', u'r', u'\u0161', u't', u' ']
+    ARNON_CHARS_IDX = {x: i for i, x in enumerate(ARNON_CHARS)}
+
+    @staticmethod
+    def _calc_corpus_dist(file):
+        prob_distr = np.zeros((len(Embedding.ARNON_CHARS), len(Embedding.VOWELS)))
+        with codecs.open(file, encoding='utf-8') as f:
+            lines = f.readlines()
+            for line in lines:
+                line = line.rstrip()
+                if line.startswith(u'#') or len(line) == 0:
+                    continue
+                w = line.split(u' ')[3]
+                w = w.replace(u'-', u'')
+                for i in range(len(w) // 2):
+                    prob_distr[Embedding.ARNON_CHARS_IDX[w[i * 2]], Embedding.VOWELS_IDX[w[i * 2 + 1]]] += 1
+        prob_distr += 0.1  # smoothing
+        prob_distr = prob_distr / np.sum(prob_distr, 1)[:, np.newaxis]  # prob distrib
+        return prob_distr
+
+    @staticmethod
+    def _calc_dist(prob_distr):
+        dist = np.zeros((prob_distr.shape[0], prob_distr.shape[0]))
+        for i in range(dist.shape[0]):
+            for j in range(dist.shape[0]):
+                if i==j:
+                    continue
+                dist[i,j] = wasserstein_distance(prob_distr[i,:],prob_distr[j,:])
+        return dist
+
+    @staticmethod
+    def _calc_err(dist, eigvec, eigval, n):
+        new_rep = np.empty((n,dist.shape[0]))
+
+        if n>=dist.shape[0] or eigval[n,n]<=0:
+            return np.nan
+
+        # Fill new representation
+        for i in range(dist.shape[0]):
+            for j in range(n):
+                new_rep[j,i] = eigvec[i,j]*np.sqrt(eigval[j,j])
+
+        # calc new distances
+        new_dist = np.zeros((dist.shape[0], dist.shape[0]))
+        for i in range(new_dist.shape[0]):
+            for j in range(new_dist.shape[0]):
+                if i==j:
+                    continue
+                new_dist[i,j] = np.linalg.norm(new_rep[:,i]-new_rep[:,j])
+
+        avg_err = np.sum(np.sum(np.abs(new_dist-dist)))/2
+
+        return avg_err
+
+
+if __name__ == '__main__':
+    emb = Embedding()
+    emb.learn('../data/HaaretzOrnan_annotated.txt')
+    emb.save('emb_model')
+    print(emb.len())
+    print(emb['t'])

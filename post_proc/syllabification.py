248f5afbefff091c6791f61640bfb26f9036cf2a
albert-shalumov <shalumov.albert@gmail.com>
2019-09-19 19:51:05 +0300

    Removed old code

--- post_proc/syllabification.py
+++ post_proc/syllabification.py
@@ -12,29 +12,3 @@ def syllabification(word):
             syllable = 0
     return ("".join(word))
 
-def test_syllabification(file_in, file_out):
-    errors = []
-    with codecs.open(file_in, encoding='utf-8') as f:
-        lines = f.readlines()
-        for line in lines:
-            line = line.rstrip()
-            if len(line) == 0:
-                continue
-            if line.startswith(u'#'):
-                line = line.split()
-                if line[1] == 'sent_id':
-                    sent_id = line[3]
-                continue
-            line = line.split()
-            original_word = line[3]
-            word = syllabification(original_word.replace("-", ""))
-            if word != original_word:
-                errors.append([sent_id, line[0], word, original_word])
-
-    with codecs.open(file_out, encoding='utf-8', mode='w') as f:
-        for i, _ in enumerate(errors):
-            f.write('sent_id:{} word:{}\nHuman  :{}\nMachine:{}\n\n'.format(errors[i][0], errors[i][1], errors[i][3], errors[i][2]))
-
-
-if __name__ == '__main__':
-    test_syllabification('../data/HaaretzOrnan_annotated.txt', 'errors.txt')
